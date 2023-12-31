81d7b50cc436d0c878f12f6abc3fce5694da9e02
valdesius <v.inc@inbox.ru>
2023-12-31 11:32:24 +0300

    v1 created tables with testing values without relationship

--- src/main/java/com/vinc/App.java
+++ src/main/java/com/vinc/App.java
@@ -1,14 +1,21 @@
 package com.vinc;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(App.class, args);
-
+        try {
+            Console.main();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
